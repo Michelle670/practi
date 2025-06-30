74acf83c79128e766ddca9b8ce87889d654bdb0d
MichelleGB <michelleguerrerobrenes@hotmail.com>
2025-06-29 20:26:15 -0600

    primer version de codigo

---
+++ src/main/java/com/mycompany/practi/Practi.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practi;
+
+
+public class Practi {
+
+    public static void main(String[] args) {
+        System.out.println("Hola tío");
+    }
+}
